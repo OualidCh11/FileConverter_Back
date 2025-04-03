0253beb36aec75dea29969ccfddc965449f54610
CHOUKRI OUALID (Stagiaire) <ou.choukri@attijariwafa.com>
2025-04-03 14:26:23 +0000

    add service configmapping and mapping

--- src/main/java/com/attw/fileConverter/service/interfqce/ConfigMappingService.java
+++ src/main/java/com/attw/fileConverter/service/interfqce/ConfigMappingService.java
@@ -1,4 +1,8 @@
 package com.attw.fileConverter.service.interfqce;
 
+import com.attw.fileConverter.dto.ConfigMappingDTO;
+import com.attw.fileConverter.model.ConfigMappingDetail;
+
 public interface ConfigMappingService {
+    ConfigMappingDetail saveConfigMapping(ConfigMappingDTO configMappingDTO);
 }
--- src/main/java/com/attw/fileConverter/service/interfqce/FileDetailService.java
+++ src/main/java/com/attw/fileConverter/service/interfqce/FileDetailService.java
@@ -1,11 +1,9 @@
 package com.attw.fileConverter.service.interfqce;
 
-import com.attw.fileConverter.model.FileDetail;
 import com.attw.fileConverter.model.FileEntity;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
 
 
 public interface FileDetailService {
