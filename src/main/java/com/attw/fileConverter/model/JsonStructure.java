fe2da79bf1aa994c3c3affb278fb373211c4a3f6
CHOUKRI OUALID (Stagiaire) <ou.choukri@attijariwafa.com>
2025-05-05 14:43:29 +0100

    create entity json structure

---
+++ src/main/java/com/attw/fileConverter/model/JsonStructure.java
@@ -0,0 +1,24 @@
+package com.attw.fileConverter.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "json_structure")
+public class JsonStructure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String keyPath;
+    private String fileDestination;
+    private LocalDateTime dateCreated;
+}
